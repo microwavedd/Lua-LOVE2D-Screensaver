15f29155c48590ec16db1c73da8aeb87a690a4d0
Microondas <143319861+microwavedd@users.noreply.github.com>
2024-04-22 20:11:22 +0200

    Comments made

--- LuaScreenSaver/main.lua
+++ LuaScreenSaver/main.lua
@@ -1,13 +1,14 @@
--- Load the Lua symbol image
+-- Load the Lua symbol image (You can change it to whatever you like. I recommend 200 pixels wide and 200 pixels height, but it's up to you.)
 function love.load()
     lua_symbol = love.graphics.newImage("lua_symbol.png")
     
     -- Set the initial window size
     setWindowSize(1920, 1080)
     
-    -- Initialize variables
-    x = 960  -- Half of 1920
-    y = 540  -- Half of 1080
+    x = 960  -- Half of 1920 ;))
+    y = 540  -- Half of 1080 ;))))
+
+    -- The speed is variable, you can configure it as you like
     speed_x = 300
     speed_y = 200
     symbol_width = 100
@@ -15,11 +16,11 @@ function love.load()
 end
 
 function love.update(dt)
-    -- Move the symbol
+    -- 
     x = x + speed_x * dt
     y = y + speed_y * dt
     
-    -- Bounce the symbol off the edges of the screen
+    -- Bounces the symbol off the edges of the screen
     if x < 0 then
         x = 0
         speed_x = -speed_x
@@ -38,7 +39,7 @@ function love.update(dt)
 end
 
 function love.draw()
-    -- Draw the Lua symbol
+    -- Draw the image you loaded (a.k.a, the default lua symbol)
     love.graphics.setColor(1, 1, 1)
     love.graphics.draw(lua_symbol, x, y)
 end
@@ -48,7 +49,7 @@ function love.conf()
 end
 
 function love.keypressed(key)
-    -- Toggle fullscreen mode when F11 is pressed
+    -- Toggle/minimize the window everytime F11 is pressed
     if key == "f11" then
         if love.window.hasFocus() then
             minimizeWindow()
@@ -62,6 +63,12 @@ function minimizeWindow()
     end
 end
 
+
 function setWindowSize(width, height)
     love.window.setMode(width, height, {resizable = true})
 end
+
+--Any improvements are welcome, create a pull request and let me know of any inefficient methods, i'm still learning lua :))
+--Quick project this time, see ya on the next one
+
+--Microwavedd ;))
